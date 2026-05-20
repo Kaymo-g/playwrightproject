5822498a8e6748990a53cceabe967e8d7462af44
Kaymo-g <kledwaba1@gmail.com>
2026-05-20 08:02:51 +0200

    updated loginPage, customFixtures and tests classes

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,13 @@
-// import {test, expect} from '@playwright/test'
+import {test, expect} from '../src/fixtures/customFixtures';
+import {validUserLoginData} from '../src/data/testData';
 
-// test('Login test', async ({page}) => {
-//     await page.goto('https://ndosisimplifiedautomation.vercel.app/')
-// })
+test.describe('Login to Ndosi Automation Website', () => {
+    test('should allow user to login with valid credentials', async ({loginPage}) => {
+        await loginPage.goto();
+        await loginPage.clickLoginButton();
+        await loginPage.emailInput.fill(validUserLoginData.admin.email);
+        await loginPage.passwordInput.fill(validUserLoginData.admin.password);
+        await loginPage.clickLoginButton();
+        // Add assertions here to verify successful login, e.g., checking for a specific element that appears after login
+    })
+})
