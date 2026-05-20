1ee554a9eba0fa2078ff8df5b00170729aaa1fdd
Kaymo-g <kledwaba1@gmail.com>
2026-05-20 10:08:57 +0200

    updated fixtures, pages and config file

---
+++ src/pages/loginPage.ts
@@ -0,0 +1,31 @@
+import { Page, Locator} from "@playwright/test";
+import { basePage } from "./basePage";
+
+export class LoginPage extends basePage {
+    private readonly pageUrl: string = 'https://ndosisimplifiedautomation.vercel.app/';
+    
+    get openLoginPage(): Locator {
+        return this.page.getByRole('button', { name: 'Login' });
+    }
+    
+    get emailInput(): Locator {
+        return this.page.getByPlaceholder('Email');
+    }
+
+    get passwordInput(): Locator {
+        return this.page.getByPlaceholder('Password');
+    }
+
+    get loginButton(): Locator {
+        return this.page.getByRole('button', { name: 'Login' });
+    }
+
+    async goto(){
+        await this.navigateTo(this.pageUrl);
+    }
+
+    async clickLoginButton(){
+        await this.clickElement(this.openLoginPage);
+    }
+
+}    
